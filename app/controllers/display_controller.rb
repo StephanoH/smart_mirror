61f41fa6b314d6f91f02a14e05607584184b6cdd
StephanoH <stephano.j.hong@gmail.com>
2017-01-12 16:22:35 -0600

    Remove geokit-rails gem and display latitude and longitude on front-end using html5 geolocation functions.

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -1,4 +1,6 @@
 class DisplayController < ApplicationController
   def index
+    location = Location.new
+    puts location.local_ip
   end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,17 @@
 class Location < ApplicationRecord
+  require 'socket'
   acts_as_mappable
-  geocode_ip_address
+
+  def local_ip
+    orig, Socket.do_not_reverse_lookup = Socket.do_not_reverse_lookup, true  # turn off reverse DNS resolution temporarily
+
+    UDPSocket.open do |s|
+      s.connect '64.233.187.99', 1
+      s.addr.last
+    end
+    ensure
+    Socket.do_not_reverse_lookup = orig
+  end
+
+
 end
