4c1c22a17ed216f0b77a1ad4fd6a59c27cf2d082
StephanoH <stephano.j.hong@gmail.com>
2017-01-17 19:08:39 -0600

    Configure render function to display (city name), (state name).

--- app/assets/javascripts/components/location.js.jsx
+++ app/assets/javascripts/components/location.js.jsx
@@ -59,10 +59,7 @@ var Location = React.createClass({
   render: function(){
     return (
       <div>
-        <h1> Latitude: {this.state.latitude} </h1>
-        <h1> Longitude: {this.state.longitude} </h1>
-        <h1> City: {this.state.city} </h1>
-        <h1> State: {this.state.state} </h1>
+        <h1>{this.state.city}, {this.state.state}</h1>
       </div>
     )
   }
