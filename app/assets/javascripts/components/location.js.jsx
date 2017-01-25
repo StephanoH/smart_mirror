5610a73ec4bc41060ac156383bda413b9be4a50b
StephanoH <stephano.j.hong@gmail.com>
2017-01-25 00:56:13 -0600

    Rename googleMapsApiURL to locationURL to better reflect the function in the context of the app.

--- app/assets/javascripts/components/location.js.jsx
+++ app/assets/javascripts/components/location.js.jsx
@@ -20,21 +20,21 @@ var Location = React.createClass({
       longitude: position.coords.longitude
     };
 
-    var googleMapsURL = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + location.latitude + ',' + location.longitude + '&key=' + this.props.googleMapsApiKey;
+    var locationURL = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + location.latitude + ',' + location.longitude + '&key=' + this.props.googleMapsApiKey;
 
     var that = this;
 
-    $.get(googleMapsURL).done(function(response) {
+    $.get(locationURL).done(function(response) {
       for (var i = 0; i < response.results[0].address_components.length; i++) {
-        
         var element = response.results[0].address_components[i];
 
         if (element.types[0] === "locality") {
           location["city"] = element.long_name;
         } else if (element.types[0] === "administrative_area_level_1") {
           location["state"] = element.short_name;
-        };
+        }
       };
+
       that.props.saveLocation(location);
     });
   },
@@ -42,7 +42,7 @@ var Location = React.createClass({
   render: function(){
     return (
         <div className="location">
-          <Weather latitude={this.props.state.latitude} longitude={this.props.state.longitude}/>
+          <Weather latitude={this.props.state.latitude} longitude={this.props.state.longitude} openWeatherMapApiKey={this.props.openWeatherMapApiKey}/>
           <h1>{this.props.state.city}, {this.props.state.state}</h1>
         </div>
     )
